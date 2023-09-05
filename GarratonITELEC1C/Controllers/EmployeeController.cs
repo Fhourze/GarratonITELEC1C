1fb8e4dc51c00e083764483bd83441a131900ff6
Fhourze <124254334+Fhourze@users.noreply.github.com>
2023-09-05 18:40:34 +0800

    Initial commit

---
+++ GarratonITELEC1C/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GarratonITELEC1C.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Employee()
+        {
+            return View();
+        }
+    }
+}
